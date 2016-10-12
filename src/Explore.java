1bc6c3f445d0dd8f73b1e70ac37a17aa64429a4d
JuliusMannes <juliusmannes@live.de>
2016-10-12 16:38:26 +0200

    Add files via upload

---
+++ src/Explore.java
@@ -0,0 +1,91 @@
+import lejos.hardware.motor.Motor;
+import lejos.robotics.subsumption.Behavior;
+
+
+public class Explore implements Behavior {
+	private UltraSonicSensor m_pLeftSensor = null;
+	private UltraSonicSensor m_pRightSensor = null;
+	private boolean m_bSuppressed = false; 
+	private float m_fFearDistance = 0.0f;
+	private float m_fClearDistance = 0.0f;
+	private float m_fSpeedModifier = 0.0f;
+	private boolean m_bInControl = false;
+
+public Explore(float a_fFearDistance, float a_fClearDistance, UltraSonicSensor a_pLeftSensor, UltraSonicSensor a_pRightSensor, float a_fSpeedModifier) { 
+	//Set variables
+	m_fFearDistance = a_fFearDistance;
+	m_fClearDistance = a_fClearDistance;
+	m_fSpeedModifier = a_fSpeedModifier;
+	
+	//Save a reference to the sensor
+	m_pLeftSensor = a_pLeftSensor;
+	m_pRightSensor = a_pRightSensor;
+}
+
+@Override
+public boolean takeControl() {
+	//Detect objects	
+	if (!m_bInControl && (m_pLeftSensor.DetectSample() < m_fFearDistance || m_pRightSensor.DetectSample() < m_fFearDistance)) {
+		//Log
+		System.out.println("Object detected, initializing Exploration!");
+		return true;
+	} 
+	//Otherwise
+	return false;
+}
+
+@Override
+public void suppress() {
+	m_bSuppressed = true;
+}
+
+//if it detects Object on the right it turns right, vice versa with left, if object in center both motors start
+@Override
+public void action() {
+	//Love started
+	boolean DriveLeft = false;
+	boolean DriveRight = false;
+	m_bSuppressed = false;
+	m_bInControl = true;
+	boolean FirstGo = true;
+	
+	//Continue driving until no longer feared or suppressed
+	while( !m_bSuppressed && (DriveLeft || DriveRight || FirstGo)) {
+		//Right motor (A)
+		float Left = m_pLeftSensor.DetectSample();
+		float Right = m_pRightSensor.DetectSample();
+		FirstGo = false;
+		
+		//Check left side
+		if(!DriveLeft && Left < m_fFearDistance) {
+			Motor.D.setSpeed(m_fSpeedModifier * Left);
+			Motor.D.backward();
+			DriveLeft = true;
+		} else if (DriveLeft && Left > m_fClearDistance) {
+			Motor.D.stop();
+			DriveLeft = false;
+		} else if (DriveLeft) {
+			Motor.D.setSpeed((int) (m_fSpeedModifier * (Left-0.01f)));
+		}
+		
+		//Check right side
+		if(!DriveRight && Right < m_fFearDistance) {
+			Motor.A.setSpeed(m_fSpeedModifier * Right);
+			Motor.A.backward();
+			DriveRight = true;
+		} else if (DriveRight && Right > m_fClearDistance) {
+			Motor.A.stop();
+			DriveRight = false;
+		} else if (DriveRight) {
+			Motor.A.setSpeed((int) (m_fSpeedModifier * (Right-0.01f)));
+		}
+	 }
+	 
+	 //Clean up
+	m_bSuppressed = false;
+	m_bInControl = false;
+	Motor.D.stop(true); 
+	Motor.A.stop(false);
+}
+
+}
